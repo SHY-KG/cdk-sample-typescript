1253f659b7b486a132bf7b6456d212badba23d14
SHY-KG <tjdghks3078etc@gmail.com>
2020-12-28 23:41:00 +0900

    HelloLambda

--- bin/cdk-sample-typescript.ts
+++ bin/cdk-sample-typescript.ts
@@ -4,4 +4,8 @@ import * as cdk from '@aws-cdk/core';
 import { CdkSampleTypescriptStack } from '../lib/cdk-sample-typescript-stack';
 
 const app = new cdk.App();
-new CdkSampleTypescriptStack(app, 'CdkSampleTypescriptStack');
+new CdkSampleTypescriptStack(app, 'CdkSampleTypescriptStack', {
+    env: {
+        region: process.env.CDK_DEFAULT_REGION,
+        account: process.env.CDK_DEFAULT_ACCOUNT
+}});
